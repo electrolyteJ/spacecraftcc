a46e8b19fce7bbadfd8fcaab457bccfbb9e6a3a3
electrolyteJ <hawks.jamesf@gmail.com>
2022-12-16 14:36:26 +0800

    u

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # install deps
 cd cmake-build-debug && conan install ..
 
-
+.conan/profiles/default
+```
+[settings]
+os=Windows
+os_build=Windows
+arch=x86_64
+arch_build=x86_64
+compiler=Visual Studio
+compiler.version=15
+build_type=Release
+```
+conan配置的arch、build_type与cmake配置的toolchain相互匹配时，工程才能跑起来。
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <glog/logging.h>
+#include <fmt/core.h>
 int main() {
     const char *tag = "spacecraftcc";
     google::InitGoogleLogging(tag);
@@ -7,5 +8,7 @@ int main() {
     LOG(INFO) << "------conan test-----------------";
     google::ShutdownGoogleLogging();
     std::cout << "Hello, World!" << std::endl;
+    std::string s = fmt::format("The answer is {}.", 42);
+    fmt::print("I'd rather be {1} than {0}.", "right", "happy");
     return 0;
 }
